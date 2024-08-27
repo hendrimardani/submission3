32e374f815176226701c8f747d927af7c713a877
henz <hendrimardani290301@gmail.com>
2024-08-27 23:39:26 +0700

    Button MainActivity.kt

--- app/src/main/java/com/example/mysubmission3/MainActivity.kt
+++ app/src/main/java/com/example/mysubmission3/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.mysubmission3
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mysubmission3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -21,6 +28,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         fullScreen()
+        binding.btnSignUp.setOnClickListener {
+
+        }
+        binding.btnSignIn.setOnClickListener {
+
+        }
     }
 
     private fun fullScreen() {
