72eb20ccc4402283633038db7363a859d7111efb
henz <hendrimardani290301@gmail.com>
2024-09-03 00:47:55 +0700

    Success Session validation on LoginActivity

--- app/src/main/java/com/example/mysubmission3/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/mysubmission3/ui/signup/SignUpActivity.kt
@@ -5,7 +5,6 @@ import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.addCallback
@@ -56,7 +55,6 @@ class SignUpActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-
     }
 
     private fun signUpButton() {
